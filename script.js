855eb155aaed3731f2b8e68b816db6ffe7b0fd64
bcorporal <bcorporal@gmail.com>
2022-03-20 20:29:24 -0400

    set variables in js

---
+++ script.js
@@ -0,0 +1,22 @@
+var score = 0;
+var allScores
+var timer = document.querySelector("#startTimer);
+var currentTime document.querySelector("#currentTime");
+var questionsSection document.querySelector("#questionsSection");
+var secondsLeft = 45;
+var penalty = 5;
+
+
+
+// var timerEL = document.getElementById('clock');
+
+// var timeLeft = 45;
+
+// function setTime() {
+//     var timeInterval = setInterval(function() {
+//         secondsLeft--;
+//     }
+// }, 60);
+// }
+
+
